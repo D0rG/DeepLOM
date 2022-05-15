0fa31af2d4aac937019957c3f09230176f6f9596
Mixail <mixail.korchikov@mail.ru>
2022-05-15 22:01:20 +0300

    Чека, управление подачек ОТВ с шлема

---
+++ Assets/SafetyRing.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class SafetyRing : MonoBehaviour
+{
+    [SerializeField] private LinearMapping linearMapping;
+    [SerializeField] private Interactable interactable;
+    [SerializeField] private Rigidbody rigidbody;
+    [SerializeField] private LinearDrive linearDrive;
+    public bool ringConnected { get; private set; }
+    private bool detached = false;
+
+    private void Awake()
+    {
+        ringConnected = true;
+    }
+
+    private void Update()
+    {
+        if (linearMapping.value >= 1f && !detached)
+        {
+            Detach();
+        }
+    }
+
+    private void Detach()
+    {
+        if (interactable.attachedToHand != null)
+        {
+            linearDrive.enabled = false;
+            interactable.attachedToHand.DetachObject(this.gameObject);
+            detached = true;
+            interactable.enabled = false;
+            gameObject.transform.parent = null;
+            rigidbody.isKinematic = false;
+            rigidbody.useGravity = true;
+
+            ringConnected = false;
+        }
+    }
+}
--- Assets/Scripts/FireExtinguisher.cs
+++ Assets/Scripts/FireExtinguisher.cs
@@ -6,24 +6,63 @@ using Valve.VR;
 
 public class FireExtinguisher : MonoBehaviour
 {
-    [SerializeField][Range(0, 1)] private float charge = 1f;
+    [SerializeField][Range(0, 1f)] private float charge = 1f;
     [SerializeField] private SteamVR_Action_Boolean pressAction;
     [SerializeField] private FireExtinguisherType extinguisherType;
+    [SerializeField] private SafetyRing safetyRing;
+    [SerializeField] private Interactable interactable;
     [SerializeField] private GameObject extrudedParticles;
-    [SerializeField] private PressureGauge pressureGauge;  
+    [SerializeField] private PressureGauge pressureGauge;
+    [SerializeField][Range(0, 10f)] private float extrusionSpeed;
+
+    [Header("Debug")]
+    [SerializeField] private bool extrude = false;  //Для проверки огнетушителей без шлема
 
     private void StartExtruding()
     {
-       extrudedParticles.SetActive(true);
+        Extruding(true);
     }
 
     private void StopExtruding()
     {
-        extrudedParticles.SetActive(false);
+        Extruding(false);
     }
 
+    private void Extruding(bool isExtruding)
+    {
+        extrudedParticles.SetActive(isExtruding);
+        extrude = isExtruding;
+    }
 
+    private void Update()
+    {
+        if (safetyRing.ringConnected) { return; }   //Кольцо предохранитель на месте
 
+        if (interactable.attachedToHand != null || extrude)
+        {
+            SteamVR_Input_Sources source = SteamVR_Input_Sources.Camera;
+
+            if (!extrude) 
+            {
+                source = interactable.attachedToHand.handType;
+            }
+            
+
+            if ((extrude || pressAction[source].stateDown) && charge > 0)
+            {
+                float delta = extrusionSpeed * Time.deltaTime;
+                if (charge - delta >= 0)
+                {
+                    charge -= delta;
+                }
+                else
+                {
+                    charge = 0;
+                }
+            }
+            pressureGauge.pressure = charge;
+        }
+    }
 }
 
 public enum FireExtinguisherType
